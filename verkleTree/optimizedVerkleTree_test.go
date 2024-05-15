365e4e177a7bf7156ffcc7175b39d8579de89dd3
Jakob-byte <70904463+Jakob-byte@users.noreply.github.com>
2024-05-15 23:21:35 +0200

    Update optimizedVerkleTree_test.go to do 3.000.000 and on for build test

--- verkleTree/optimizedVerkleTree_test.go
+++ verkleTree/optimizedVerkleTree_test.go
@@ -24,7 +24,7 @@ var fanOuts = struct {
 var certAmount = struct {
 	c []int
 }{
-	c: []int{1000000, 2000000, 3000000, 4000000, 5000000, 6000000, 7000000, 8000000, 9000000, 10000000}, //TODO change back
+	c: []int{3000000, 4000000, 5000000, 6000000, 7000000, 8000000, 9000000, 10000000}, //TODO change back, 1000000, 2000000, 
 }
 var optimizedTable = []struct {
 	fanOut int
@@ -342,7 +342,7 @@ func BenchmarkOptimizedDifferentAmountOfThreads(b *testing.B) {
 }
 
 // TODO Run benchmark on server
-// go test -bench=BenchmarkOptimizedBuildTreeTime -run=^a -benchtime=10x -benchmem  -timeout 9999999s | tee BenchmarkOptimizedBuildTreeTime.txt
+// sudo go test -bench=BenchmarkOptimizedBuildTreeTime -run=^a -benchtime=10x -benchmem  -timeout 9999999s | sudo tee BenchmarkOptimizedBuildTreeTimeV3.txt
 func BenchmarkOptimizedBuildTreeTime(b *testing.B) {
 	fmt.Println("BenchmarkOptimizedBuildTreeTime - Starting")
 	b.ResetTimer()
